ee49b36524ff59781d85bbd3cdc0a732c2c1525a
1412255151 <111180733+1412255151@users.noreply.github.com>
2023-08-19 14:45:33 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,7 @@
+lst1 = ["apple", "banana", "cherry", "mango"]
+lst2 = ("apple", "banana", "cherry", "mango")
+lst3 = [10,10.20,"apple",[1,2,3,4],(5,6,7,8),4+8j]
+lst3 = (10,10.20,"apple",[1,2,3,4],(5,6,7,8),4+8j)
+print("type of lst1",type(lst1))
+print("type of lst2",type(lst2))
+print("type of lst3",type(lst3))
